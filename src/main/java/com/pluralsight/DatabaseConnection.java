b5d96cb25c683223d10b6d33ea072b65b82b78c2
Chris Bunny <christopherjbunny@gmail.com>
2026-04-28 14:33:46 -0500

    Created a method that converts data in transactions.cvs to SQL allowing it to save in the database.

--- src/main/java/com/pluralsight/DatabaseConnection.java
+++ src/main/java/com/pluralsight/DatabaseConnection.java
@@ -2,6 +2,8 @@ package com.pluralsight;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 
 public class DatabaseConnection {
 
@@ -26,5 +28,35 @@ public class DatabaseConnection {
             e.printStackTrace();
         }
         return databaselink;
+    }
+    //Saves information from transactions.csv file to MySQL database
+    public void saveToDataBase(ArrayList<Transactions> list) {
+        String insert = "INSERT INTO transactions (date, time, description, vendor, amount) VALUES (?, ?, ?, ?, ?)";
+
+        try {
+            //calling getConnection method
+            Connection connect = getConnection();
+            //instructions allowing SQL to fill in the blanks for the ?
+            PreparedStatement prepStatement = connect.prepareStatement(insert);
+
+            /*
+            The java.sql lines are for conversion purposes for SQL
+            YYYY-MM-DD for date and HH:MM:SS for time
+            */
+            for (Transactions t : list) {
+                    prepStatement.setDate(1, java.sql.Date.valueOf(t.getDate()));
+                    prepStatement.setTime(2, java.sql.Time.valueOf(t.getTime()));
+                    prepStatement.setString(3, t.getDescription());
+                    prepStatement.setString(4, t.getVendor());
+                    prepStatement.setDouble(5, t.getAmount());
+
+                    prepStatement.execute();
+            }
+            System.out.println("Transactions have been successfully sent over to database!");
+
+            } catch(Exception e) {
+                e.printStackTrace();
+                System.out.println("Could not save to database.");
+        }
     }
 }
