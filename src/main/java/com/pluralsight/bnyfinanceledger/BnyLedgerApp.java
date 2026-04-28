6954ba440b1194792ea43370b2bae502389d6d16
Chris Bunny <christopherjbunny@gmail.com>
2026-04-27 22:29:36 -0500

    Fixed the header format. After the header when using the ledger method for displaying all transactions, the first line written was on the same line as the header, so I added bWriter.newLine() for it to write to the next line.

--- src/main/java/com/pluralsight/bnyfinanceledger/BnyLedgerApp.java
+++ src/main/java/com/pluralsight/bnyfinanceledger/BnyLedgerApp.java
@@ -18,10 +18,10 @@ public class BnyLedgerApp {
             System.out.println(("Welcome to BNY Financial Corp."));
             Thread.sleep(1000);
             System.out.println(
-                    """
+                            """
                             D) Make Deposit
                             P) Make Payment (Debit)
-                            L) Go to Ledger
+                            L) Go to ledger
                             X) Exit
                             """
             );
@@ -40,12 +40,12 @@ public class BnyLedgerApp {
                     makePayment();
                     break;
                 case "L":
-                    Ledger();
+                    ledger();
                     break;
                 case "X":
                     System.exit(0);
                 default:
-                    System.out.println("Invalid choice. Try again.");
+                    System.out.println("Invalid choice. Try again.\n");
 
             }
         }
@@ -54,6 +54,7 @@ public class BnyLedgerApp {
     //method for making a deposit
     public static void makeDeposit() {
 
+        //prompting user to enter values
         System.out.println("\nYou have selected to deposit.");
         System.out.print("Please enter a description: ");
         String userDesc = myScanner.nextLine();
@@ -78,7 +79,7 @@ public class BnyLedgerApp {
 
     //method for making a payment
     public static void makePayment() {
-        //similar
+        //similar to the makeDeposit method, prompts user
         System.out.println("\nYou have selected to make a payment.");
         System.out.print("Please enter a description: ");
         String userDesc = myScanner.nextLine();
@@ -88,16 +89,78 @@ public class BnyLedgerApp {
         double userAmount = myScanner.nextDouble();
         myScanner.nextLine();
 
+        //if the amount the user enters is positive, it forces the input to be negative
         if (userAmount > 0) {
-            //forcing the input to be negative if user enters a positive number
             userAmount *= -1;
         }
+        //writes values of object payment to the transactions.csv file
         Transactions payment = new Transactions(LocalDate.now(), LocalTime.now(), userDesc, userVendor, userAmount);
         FileManager.writeToFile(payment);
         System.out.println("Your payment was successful!");
     }
 
-    public static void Ledger() {
+    //method for the ledger opener
+    public static void ledger() throws InterruptedException {
+
+        //similar to the home menu, just with different options
+        boolean ledgerOpen = true;
+
+        while(ledgerOpen) {
+            System.out.println("Bny ledger");
+            Thread.sleep(1000);
+            System.out.println(
+                    """
+                    A) All
+                    D) Deposits
+                    P) Payments
+                    R) Reports
+                    H) Home
+                    """
+            );
+            Thread.sleep(1000);
+            System.out.println("Select one of the options above.\n");
+            System.out.print("Selection: ");
+            String command = myScanner.nextLine().toUpperCase();
+
+            ArrayList<Transactions> all = FileManager.loadTransactions();
+
+            switch(command) {
+                case "A":
+                    displayAll(all);
+                    break;
+                case "D":
+                    displayDeposits();
+                    break;
+                case "P":
+                    displayPayments();
+                case "R":
+                    reports();
+                    break;
+                case "H":
+                    home();
+                    break;
+                default:
+                    System.out.println("Invalid Choice. Try again.\n");
+            }
+        }
+    }
+    public static void displayAll(ArrayList<Transactions> list) {
+        System.out.printf("%s | %s | %s | %s | %s\n", "Date", "Time", "Description", "Vendor", "Amount");
+
+        for (Transactions t : list) {
+            System.out.printf("%s | %s | %s | %s | %.2f\n", t.getDate(), t.getTime(), t.getDescription(), t.getVendor(), t.getAmount());
+        }
+    }
+    public static void displayDeposits() {
+
+    }
+    public static void displayPayments() {
+
+    }
+    public static void reports() {
+
+    }
+    public static void home() {
 
     }
 }
--- src/main/java/com/pluralsight/bnyfinanceledger/FileManager.java
+++ src/main/java/com/pluralsight/bnyfinanceledger/FileManager.java
@@ -5,15 +5,14 @@ import java.time.LocalTime;
 import java.util.*;
 
 public class FileManager {
-    //creating a shortcut so i dont have to type the pathname out
+    //creating a shortcut, so I don't have to type the pathname out everytime
     static File file = new File("src/main/resources/transactions.csv");
 
     //method for writing to file
     public static void writeToFile(Transactions transaction) {
 
-
         try {
-            //empytFile is true if transaction file is empty otherwise it will be false
+            //emptyFile is true if transaction file is empty otherwise it will be false
             //using this to add header to file if file is empty
             boolean emptyFile = file.length() == 0;
 
@@ -22,7 +21,8 @@ public class FileManager {
 
             //writes the header into the file if the file is empty
             if (emptyFile) {
-                bWriter.write("Date|Time|Description|Vendor|Amount\n");
+                bWriter.write("Date|Time|Description|Vendor|Amount");
+                bWriter.newLine();
             }
 
             //writes the information in format from the method toFile() in transaction class
@@ -39,12 +39,13 @@ public class FileManager {
         }
     }
 
-    public static List<Transactions> loadTransactions() {
-        List<Transactions> list = new ArrayList<>();
+    //method for loading the transactions into an array list and formatting it
+    public static ArrayList<Transactions> loadTransactions() {
+        ArrayList<Transactions> list = new ArrayList<>();
 
         try {
             //buffered reader and file reader
-            BufferedReader bReader = new BufferedReader(new FileReader("src/main/resources/transactions.csv"));
+            BufferedReader bReader = new BufferedReader(new FileReader(file));
 
             //variable that will later contain the read method
             String line;
@@ -52,23 +53,25 @@ public class FileManager {
             //skips the header I made
             bReader.readLine();
 
-            //reads each line from the file while its not empty and splits by pipe
+            //reads each line from the file while it's not empty and splits by pipe
             while ((line = bReader.readLine()) != null) {
                 String[] parts = line.split("\\|");
 
+                //parses the data by index
                 LocalDate date = LocalDate.parse(parts[0]);
                 LocalTime time = LocalTime.parse(parts[1]);
                 String desc = parts[2];
                 String vendor = parts[3];
                 double amount = Double.parseDouble(parts[4]);
 
+                //storing the values into object t
                 Transactions t = new Transactions(date, time, desc, vendor, amount);
 
+                //adding to array list
                 list.add(t);
             }
             bReader.close();
-
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println("No file found yet.");
         }
 
